b7eb2c449668aad0b48d64ac76dc9ac3426343f0
user <user@dell>
2018-06-03 11:10:30 +0530

    1st commit

---
+++ maven1/src/test/java/pom/Googlepo.java
@@ -0,0 +1,18 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import library.Basepage;
+
+public class Googlepo extends Basepage{
+	public Googlepo(WebDriver driver) {
+		super(driver);
+		}
+	@FindBy(name="q")
+	private WebElement searchTxtBx;
+	public WebElement searchTxtBx(){
+		return searchTxtBx;
+	}
+}
---
+++ maven1/src/test/java/pom/Searchpo.java
@@ -0,0 +1,34 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import library.Basepage;
+
+public class Searchpo extends Basepage {
+	public Searchpo(WebDriver driver) {
+		super(driver);
+		}
+	@FindBy(xpath="//a[text()='Facebook - Log In or Sign Up']")
+	private  WebElement facebookLink;
+	public   WebElement facebookLink() {
+		return facebookLink;
+	}
+	@FindBy(xpath="//a[text()='Instagram']")
+	private  WebElement instaLink;
+	public   WebElement instaLink() {
+		return instaLink;
+	}
+	@FindBy(xpath="//a[text()='Login on Twitter']")
+	private  WebElement twitterLink;
+	public   WebElement twitterLink() {
+		return twitterLink;
+	}
+	@FindBy(xpath="//a[text()='Sign Up | LinkedIn']")
+	private  WebElement linekdinLink;
+	public   WebElement linkedinLink() {
+		return linekdinLink;
+	}
+
+}
